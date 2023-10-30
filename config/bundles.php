6cb2f78bb784af2bf90e1106958f9184e17ab495
Shanazar <snizamaddinow@gmail.com>
2023-10-30 22:49:21 +0300

    feature: easy admin installed.

--- config/bundles.php
+++ config/bundles.php
@@ -23,4 +23,5 @@ return [
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
