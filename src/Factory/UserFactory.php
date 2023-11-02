2ced30b5d5e576b96a3d8d01802588c07acfe75e
Shanazar <snizamaddinow@gmail.com>
2023-11-02 23:45:39 +0300

    feature: user field changes

--- src/Factory/UserFactory.php
+++ src/Factory/UserFactory.php
@@ -40,8 +40,9 @@ final class UserFactory extends ModelFactory
             // TODO add your default values here (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories)
             'email' => self::faker()->email(),
             'firstName' => self::faker()->firstName(),
+            'lastName' => self::faker()->lastName(),
             'plainPassword' => 'tada',
-            'isVerified' => true,
+            'isVerified' => self::faker()->boolean(),
         ];
     }
 
